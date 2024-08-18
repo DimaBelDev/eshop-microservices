8dee37591e8e7441531271c891ea2c41e2ebaa01
salita_dima <salitadima1@gmail.com>
2024-08-18 12:54:57 +0200

    Added Circuit Breaker

--- order-service/src/main/java/com/salita/orderservice/controller/OrderController.java
+++ order-service/src/main/java/com/salita/orderservice/controller/OrderController.java
@@ -3,6 +3,7 @@ package com.salita.orderservice.controller;
 
 import com.salita.orderservice.dto.OrderRequest;
 import com.salita.orderservice.service.OrderService;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -16,9 +17,14 @@ public class OrderController {
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
+    @CircuitBreaker(name = "inventory", fallbackMethod = "fallbackMethod")
     public String placeOrder(@RequestBody OrderRequest orderRequest) {
         orderService.placeOrder(orderRequest);
         return "Order Placed Successfully";
+    }
+
+    public String fallbackMethod(OrderRequest orderRequest, RuntimeException runtimeException) {
+        return "Oops! Something went wrong! please order after some time!";
     }
 
 
